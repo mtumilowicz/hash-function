a4010e40a15a34e7ffb003ad195230acb319bc5a
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-06-26 00:23:40 +0200

    formatting and visibility fix

--- src/main/java/StringHashFunction.java
+++ src/main/java/StringHashFunction.java
@@ -3,17 +3,16 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * Created by mtumilowicz on 2018-06-24.
  */
-public final class StringHashFunction {
+final class StringHashFunction {
     private static final int prime = 31;
-    
-    
+
     private StringHashFunction() {
     }
-    
-    public static int hash(String string) {
+
+    static int hash(String string) {
         return StringUtils.defaultIfEmpty(string, StringUtils.EMPTY)
                 .chars()
-                .reduce((x, y) -> 31 * x + y)
+                .reduce((x, y) -> prime * x + y)
                 .orElse(0);
     }
 }
